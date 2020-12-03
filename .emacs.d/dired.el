d0b4a0ce995254bb71b777cb6bcd7248d6f02db3
Vince Levey <vincel@vincel.org>
2020-12-03 18:50:18 +0000

    Group directories first in dired

--- .emacs.d/dired.el
+++ .emacs.d/dired.el
@@ -1,18 +1,24 @@
 ;;; -*- lexical-binding: t -*-
-(when (eq system-type 'darwin)
-  (setq dired-guess-shell-alist-user
-        (list
-         (list "\\.pdf\\'"   "open")
-         (list "\\.gif\\'"   "open")
-         (list "\\.jpe?g\\'" "open")
-         (list "\\.png\\'"   "open")
-         (list "\\.tiff\\'"  "open")
-         ))
+(use-package dired
+  :ensure nil
+  :config
+  (progn
+    (when (eq system-type 'darwin)
+      (setq dired-guess-shell-alist-user
+            (list
+             (list "\\.pdf\\'"   "open")
+             (list "\\.gif\\'"   "open")
+             (list "\\.jpe?g\\'" "open")
+             (list "\\.png\\'"   "open")
+             (list "\\.tiff\\'"  "open")
+             ))
 
-  ;; dired use GNU ls
-  (let ((gls "/usr/local/opt/coreutils/bin/gls"))
-    (if (file-exists-p gls)
-        (setq
-         insert-directory-program gls
-         dired-use-ls-dired t))
+      ;; dired use GNU ls
+      (let ((gls "/usr/local/opt/coreutils/bin/gls"))
+        (if (file-exists-p gls)
+            (setq
+             insert-directory-program gls
+             dired-listing-switches "-alGhv --group-directories-first"
+             dired-use-ls-dired t))
+        ))
     ))
