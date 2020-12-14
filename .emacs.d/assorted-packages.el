ff0e3f4f024ef681a493e4d44ad7f734ae627de2
Vince Levey <vincel@vincel.org>
2020-12-14 15:29:00 +0000

    Add terraform-docs boilerplate

--- .emacs.d/assorted-packages.el
+++ .emacs.d/assorted-packages.el
@@ -1,16 +1,9 @@
 ;;; -*- lexical-binding: t -*-
 
-(use-package all-the-icons
-  :if window-system
+(use-package ansible
+  :ensure yaml-mode
   :config
-  (when (not (member "all-the-icons" (font-family-list)))
-    (all-the-icons-install-fonts t)))
-
-(use-package spacemacs-common
-  :if window-system
-  :ensure spacemacs-theme
-  :config
-  (load-theme 'spacemacs-dark t nil))
+  (add-hook 'yaml-mode-hook #'(lambda () (ansible 1))))
 
 (use-package bind-key
   :bind (("C-x C-b" . buffer-menu)
@@ -22,33 +15,39 @@
          ))
 
 (use-package ipcalc)
-(use-package markdown-mode)
 
 (use-package json-mode
   :pin gnu)
 
-(use-package yaml-mode)
+(use-package magit)
 
-(use-package ansible
-  :ensure yaml-mode
-  :config
-  (add-hook 'yaml-mode-hook #'(lambda () (ansible 1))))
+(use-package markdown-mode)
 
 (use-package smartparens
   :ensure yaml-mode
   :config
   (add-hook 'yaml-mode-hook #'smartparens-mode))
 
-(use-package terraform-mode
+(use-package spacemacs-common
+  :if window-system
+  :ensure spacemacs-theme
   :config
+  (load-theme 'spacemacs-dark t nil))
+
+(use-package terraform-mode
+  :init
+  (defun terraform-mode-hackery ()
+    (cond ((zerop (buffer-size))
+           (let* ((mod (buffer-modified-p)))
+             (insert
+              "/**\n"
+              "* ## Title: level 2 heading\n"
+              "*\n"
+              "* Description\n"
+              "*/\n")
+             (set-buffer-modified-p mod))))
+    nil)
+  (add-hook 'terraform-mode-hook #'terraform-mode-hackery)
   (add-hook 'terraform-mode-hook #'terraform-format-on-save-mode))
 
-;; (use-package puppet-mode
-;;   :config
-;;   (add-hook 'puppet-mode-hook
-;;             (lambda ()
-;;               (add-to-list 'write-file-functions 'delete-trailing-whitespace)
-;;               )))
-
-;; (use-package magit
-;;   :bind (("C-x m" . magit-status)))
+(use-package yaml-mode)
