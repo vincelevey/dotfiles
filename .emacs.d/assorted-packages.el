2fcccf9ca916d9fc3df3a5987e6f5ade3860c8b2
Vince Levey <vincel@vincel.org>
2021-11-10 15:32:46 +0000

    Use dired-sidebar again
    I'd resisted this, since of late it feels like I'm turning Emacs into
    VS Code, however it is a very useful package.

--- .emacs.d/assorted-packages.el
+++ .emacs.d/assorted-packages.el
@@ -41,6 +41,21 @@
         ))
     ))
 
+(use-package dired-sidebar
+  :bind (("C-x C-n" . dired-sidebar-toggle-sidebar))
+  :ensure t
+  :commands (dired-sidebar-toggle-sidebar)
+  :init
+  (add-hook 'dired-sidebar-mode-hook
+            (lambda ()
+              (unless (file-remote-p default-directory)
+                (auto-revert-mode))))
+  :config
+  (push 'toggle-window-split dired-sidebar-toggle-hidden-commands)
+  (push 'rotate-windows dired-sidebar-toggle-hidden-commands)
+  (setq dired-sidebar-theme 'nerd)
+  (setq dired-sidebar-use-custom-font t))
+
 (use-package dockerfile-mode
   :mode "Dockerfile\\'")
 
